69a39529273b66e022fd25448bd521c36e28c4bf
ollir <olli.rauramo@aalto.fi>
2017-06-26 14:20:01 +0300

    initial commit, boilerplate

---
+++ webpack.config.prod.js
@@ -0,0 +1,42 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    './app/app'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist/static'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    //new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': "'production'"
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compressor: {
+        warnings: false
+      }
+    })
+  ],
+  module: {
+    loaders: [
+    {
+      test: /\.(js|jsx)$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'app')
+    },
+    {
+      test: /\.scss$/,
+      include: path.join(__dirname, 'app'),
+      loader: 'style-loader!css-loader!sass-loader'
+    }
+    ]
+  }
+};
