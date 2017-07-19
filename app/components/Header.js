14eed6136335a65c07c1cc57bc0544785aa21ef1
ollir <olli.rauramo@aalto.fi>
2017-07-19 13:18:50 +0300

    add nav to header component

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router';
 
+import Nav from './Nav';
+
 const Header = () => {
   return (
     <div>
+      <Nav />
       <h1>Scenario building</h1>
       <p>This tool helps you in building scenarios.</p>
-      <a href="/login">Login</a>
-      <a href="/logout">Logout</a>
     </div>
   )
 }
