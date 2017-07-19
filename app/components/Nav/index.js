e4916f0098ff99bcc0642a2ebf625f6689c97b5c
ollir <olli.rauramo@aalto.fi>
2017-07-19 13:18:35 +0300

    add simple nav component

---
+++ app/components/Nav/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+import { connect } from 'react-redux';
+import store from '../../store';
+
+
+class Nav extends React.Component {
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    const logInOut = (loggedIn) => {
+      if (loggedIn) {
+        return <a className="Nav__link" href="/logout">Logout</a>
+      } else {
+        return <a className="Nav__link" href="/login">Login</a>
+      }
+    }
+
+    return(
+      <nav className="Nav">
+        <div className="Nav__container">
+          <Link to="/" className="Nav__brand">
+            Home
+          </Link>
+          <div className="Nav__right">
+            <ul className="Nav__item-wrapper">
+              <li className="Nav__item">
+                {logInOut(this.props.auth)}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    auth: state.auth
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Nav);
