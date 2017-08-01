8d4c3f706b7f6b5fdc60a910982d72b3b4f648c7
ollir <olli.rauramo@aalto.fi>
2017-08-01 10:24:19 +0300

    rename individual tools to templates for clarity

--- app/components/ApplicationMenu/ApplicationMenuView.js
+++ app/components/ApplicationMenu/ApplicationMenuView.js
@@ -8,25 +8,25 @@ const ApplicationMenuView = ({props}) => {
       <h1>Scenario building applications</h1>
       <div className="responsive-layout">
         <div className="responsive-layout__column">
-          <a href="http://www.s3platform.eu/wp-content/uploads/2017/07/Guideline_Tool_Scenario-Building_MR-2017-31-07.pdf"><button className="button button--menu">Application Guide PDF</button></a>
+          <a href="http://www.s3platform.eu/wp-content/uploads/2017/07/Guideline_Tool_Scenario-Building_MR-2017-31-07.pdf"><button className="button button--menu">Application guide PDF</button></a>
         </div>
         <div className="responsive-layout__column">
-          <a href="http://www.s3platform.eu/wp-content/uploads/2017/07/Pest_pdf_template.pdf"><button className="button button--menu">PEST-Analysis Tool</button></a>
+          <a href="http://www.s3platform.eu/wp-content/uploads/2017/07/Pest_pdf_template.pdf"><button className="button button--menu">PEST-analysis template</button></a>
         </div>
         <div className="responsive-layout__column">
-          <Link to="/application"><button className="button button--menu">Impact Analysis Tool</button></Link>
+          <Link to="/application"><button className="button button--menu">Impact analysis template</button></Link>
         </div>
         <div className="responsive-layout__column">
-          <a href="http://www.s3platform.eu/wp-content/uploads/2017/07/Scenario_building_template.pdf"><button className="button button--menu">Scenario Building Template</button></a>
+          <a href="http://www.s3platform.eu/wp-content/uploads/2017/07/Scenario_building_template.pdf"><button className="button button--menu">Scenario building template</button></a>
         </div>
         <div className="responsive-layout__column">
-          <a href="http://www.s3platform.eu/wp-content/uploads/2017/07/Scenario_description_template.pdf"><button className="button button--menu">Scenario Description Tool</button></a>
+          <a href="http://www.s3platform.eu/wp-content/uploads/2017/07/Scenario_description_template.pdf"><button className="button button--menu">Scenario description template</button></a>
         </div>
         <div className="responsive-layout__column">
-          <a href="http://www.s3platform.eu/wp-content/uploads/2017/07/Scenario_description_images_template.pdf"><button className="button button--menu">Scenario Description Tool with Image Boxes</button></a>
+          <a href="http://www.s3platform.eu/wp-content/uploads/2017/07/Scenario_description_images_template.pdf"><button className="button button--menu">Scenario description template with image boxes</button></a>
         </div>
         <div className="responsive-layout__column">
-          <a href="http://www.s3platform.eu/wp-content/uploads/2017/07/Preparation_plan_template.pdf"><button className="button button--menu">Preparation Plan Tool</button></a>
+          <a href="http://www.s3platform.eu/wp-content/uploads/2017/07/Preparation_plan_template.pdf"><button className="button button--menu">Preparation plan template</button></a>
         </div>
       </div>
     </section>
