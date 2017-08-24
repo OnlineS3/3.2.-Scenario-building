76654fd744cd92bf38b039b37db523d917e6c499
meritmorikawa <30862225+meritmorikawa@users.noreply.github.com>
2017-08-24 12:31:25 +0300

    second guideline to related documents

---
+++ app/components/ContentPage/content/RelatedDocumentsView.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+
+const RelatedDocuments = () => {
+
+  return (
+    <div>
+      <div>
+		<h1>Useful resources for identifying uncertainties and trends:</h1>
+		<p><a href="http://wiwe.iknowfutures.eu/what-is-a-weak-signal/">iKnow</a> - Database on wildcards and weak signals, unfortunately not updated recently<br>
+		<a href="http://ec.europa.eu/growth/smes/cluster/observatory/european-cluster-trend-report_en">European Cluster Trend Report</a><br>
+		<a href="http://www.thegff.com/">The Global Futures and Foresight - Reports on future studies</a></p>
+		
+		<h1>Statistics databases:</h1>
+		<p><a href="http://ec.europa.eu/eurostat">Eurostat</a><br>
+		<a href="http://www.oecd.org/eu/">OECD Europe Statistics</a><br>
+		<a href="https://data.europa.eu/euodp/en/data">European Union Open Data Portal</a><br>
+		The Internet is full of free statistics sites if they seem helpful.</p>
+		
+		<h1>Commercial software tools for foresight:</h1>
+		<p><a href="http://www.futuresplatform.com/">Futures Platform</a><br>
+		<a href="https://www.itonics.de/software/itonics-strategic-foresight/">ITONICS Software</a></p>
+
+		<h1>External documents for understanding scenario building</h2>
+		<p><a href="http://forlearn.jrc.ec.europa.eu/guide/4_methodology/meth_scenario.htm">The JRC European Commission FOR-LEARN Guide for Scenario Building</a><br>
+		<a href="https://www.slideshare.net/adgo/scenario-building-workshop-how-to-build-and-use-scenarios">Scenario Building Workshop - How to Build and Use Scenarios by Adam Gordon</a><br>
+		<a href="http://webarchive.nationalarchives.gov.uk/20140108141323/http:/www.bis.gov.uk/assets/foresight/docs/horizon-scanning-centre/foresight_scenario_planning.pdf">Scenario Planning Guide by the Government Office of Science (UK)</a></p>
+		
+		<h1>Selected use cases using the Scenario building toolset of the Online-S3 platform:</h1>
+		<p>Will be uploaded soon</p>
+		<h1>Selected RIS3 strategies including the Scenario method in their implementation process:</h1>
+		<p>Will be uploaded soon</p>
+	   </div>
+    </div>
+  )
+}
+
+
+export default RelatedDocuments;
