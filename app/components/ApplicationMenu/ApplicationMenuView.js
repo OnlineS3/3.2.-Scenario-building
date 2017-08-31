7593ab8807232c7346c50487c4f4e75ca2b4d805
meritmorikawa <30862225+meritmorikawa@users.noreply.github.com>
2017-08-31 13:03:42 +0300

    Added 1 button
    Added scenario building process template and updated pdf guide link.

--- app/components/ApplicationMenu/ApplicationMenuView.js
+++ app/components/ApplicationMenu/ApplicationMenuView.js
@@ -8,8 +8,11 @@ const ApplicationMenuView = ({props}) => {
       <h1>Scenario building applications</h1>
       <div className="responsive-layout">
         <div className="responsive-layout__column">
-          <a href="http://www.s3platform.eu/wp-content/uploads/2017/07/Guideline_Tool_Scenario-Building_MR-2017-31-07.pdf"><button className="button button--menu">Application guide PDF</button></a>
+          <a href="http://www.s3platform.eu/wp-content/uploads/2017/08/Guideline_Tool_Scenario-Building_MR-2017-21-08.pdf"><button className="button button--menu">Application guide PDF</button></a>
         </div>
+    <div className="responsive-layout__column">
+      <a href="http://www.s3platform.eu/wp-content/uploads/2017/08/Scenario-process-template.pdf"><button className="button button--menu">Scenario building process template</button></a>
+    </div>
         <div className="responsive-layout__column">
           <a href="http://www.s3platform.eu/wp-content/uploads/2017/07/Pest_pdf_template.pdf"><button className="button button--menu">PEST-analysis template</button></a>
         </div>
