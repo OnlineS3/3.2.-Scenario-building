46cc65b48c66730fa3641396f199400b836897c2
ollir <olli.rauramo@aalto.fi>
2017-11-03 16:02:31 +0200

    remove redundant links to templates from sidebar

--- app/components/ContentPage/SideBar.js
+++ app/components/ContentPage/SideBar.js
@@ -15,13 +15,7 @@ const SideBar = ({props}) => {
   return (
     <aside style={style}>
       <SideBarButton text="Download guide" alt={true} link="http://www.s3platform.eu/wp-content/uploads/2017/08/Guideline_Tool_Scenario-Building_MR-2017-21-08.pdf"></SideBarButton>
-      <SideBarButton text="Scenario building process template" link="http://www.s3platform.eu/wp-content/uploads/2017/08/Scenario-process-template.pdf"></SideBarButton>
-      <SideBarButton text="PEST-analysis template" link="http://www.s3platform.eu/wp-content/uploads/2017/07/Pest_pdf_template.pdf"></SideBarButton>
-      <SideBarButton text="Impact analysis template" link="/application"></SideBarButton>
-      <SideBarButton text="Scenario building template" link="http://www.s3platform.eu/wp-content/uploads/2017/07/Scenario_building_template.pdf"></SideBarButton>
-      <SideBarButton text="Scenario description template" link="http://www.s3platform.eu/wp-content/uploads/2017/07/Scenario_description_template.pdf"></SideBarButton>
-      <SideBarButton text="Scenario description template with image boxes" link="http://www.s3platform.eu/wp-content/uploads/2017/07/Scenario_description_images_template.pdf"></SideBarButton>
-      <SideBarButton text="Preparation plan template" link="http://www.s3platform.eu/wp-content/uploads/2017/07/Preparation_plan_template.pdf"></SideBarButton>
+      <SideBarButton text="Templates" link="/applications"></SideBarButton>
     </aside>
   )
 
