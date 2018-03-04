cd11f3f07b31941e36775c4a65b1f66c3e955a99
Henrik Aarnio <hjaarnio@gmail.com>
2018-03-05 01:39:37 +0200

    add latest guide

--- app/components/ContentPage/SideBar.js
+++ app/components/ContentPage/SideBar.js
@@ -14,7 +14,7 @@ const SideBar = ({props}) => {
   }
   return (
     <aside style={style}>
-      <SideBarButton text="Download guide" alt={true} link="http://www.s3platform.eu/wp-content/uploads/2017/08/Guideline_Tool_Scenario-Building_MR-2017-21-08.pdf"></SideBarButton>
+      <SideBarButton text="Download guide" alt={true} link="http://www.s3platform.eu/wp-content/uploads/Guideline_Tool_Scenario-Building_MR-2017-10-31.pdf"></SideBarButton>
       <SideBarButton text="Access to Application" link="/applications"></SideBarButton>
     </aside>
   )
